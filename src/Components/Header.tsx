3ba8666af01561682bf938bfd3a966771e14a094
natgit8 <natasha.gitlin8@gmail.com>
2021-03-11 16:41:14 -0500

    update

---
+++ src/Components/Header.tsx
@@ -0,0 +1,7 @@
+const navLinks = [
+  { title: `Home`, path: `/` },
+  { title: `Film Trivia`, path: `/film_trivia` },
+  { title: `Log In`, path: `/logged_in` },
+  { title: `contact`, path: `/contact` },
+  { title: `faq`, path: `/faq` },
+]
